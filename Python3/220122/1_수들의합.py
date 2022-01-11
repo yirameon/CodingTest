0227a98d0be9fc67970c9a1b53f6ca162abc9441
yirameon <96986153+yirameon@users.noreply.github.com>
2022-01-11 23:04:00 +0900

    Update 1_수들의합.py

--- Python3/220122/1_수들의합.py
+++ Python3/220122/1_수들의합.py
@@ -1,1 +1,13 @@
-.
+S = int(input())
+num=0
+for i in range(1,4294967295):
+    num=num+i
+    if S==1:
+        print(1)
+        break
+    elif S<num:
+        print(i-1)
+        break
+    elif S==sum:
+        print(i)
+        break  
