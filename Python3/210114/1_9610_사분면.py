f1f2b5816d8028b3abbb4eeb888e5d213ba8b68a
yirameon <96986153+yirameon@users.noreply.github.com>
2022-01-15 00:11:35 +0900

    210114 코딩테스트 준비 코드 업로드(사분면)

---
+++ Python3/210114/1_9610_사분면.py
@@ -0,0 +1,19 @@
+axis=q1=q2=q3=q4=0
+n=int(input())
+for i in range(n):
+    x,y=map(int,input().split())
+    if x==0 or y==0:
+        axis+=1
+    elif x>0 and y>0:
+        q1+=1
+    elif x<0 and y>0:
+        q2+=1
+    elif x<0 and y<0:
+        q3+=1
+    elif x>0 and y<0:
+        q4+=1
+print('Q1: %d'%q1)
+print('Q2: %d'%q2)
+print('Q3: %d'%q3)
+print('Q4: %d'%q4)
+print('AXIS: %d'%axis)
