80855b8103446240850647a2687e30a8a79a2407
yirameon <96986153+yirameon@users.noreply.github.com>
2022-02-02 00:46:08 +0900

    220201 백준 코딩테스트 코드 업로드(미로탈출)

---
+++ Python3/220201/2_2178_미로탐색.py
@@ -0,0 +1,24 @@
+from collections import deque
+N,M=map(int,input().split())
+graph=[]
+for _ in range(N):
+    graph.append(list(map(int,input())))
+dx=[-1,1,0,0]
+dy=[0,0,-1,1]
+def BFS(x,y):
+    Q=deque()
+    Q.append((x,y))
+    while Q:
+        x,y=Q.popleft()
+        for i in range(4):
+            nx=x+dx[i]
+            ny=y+dy[i]
+            if nx<0 or nx>=N or ny<0 or ny>=M:
+                continue
+            if graph[nx][ny]==0:
+                continue
+            if graph[nx][ny]==1:
+                graph[nx][ny]=graph[x][y]+1
+                Q.append((nx,ny))
+    return graph[N-1][M-1]
+print(BFS(0,0))            
