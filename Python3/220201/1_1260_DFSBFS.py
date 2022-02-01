69ae088fdcf562417400b2b46cb247461b0c0694
yirameon <96986153+yirameon@users.noreply.github.com>
2022-02-02 00:45:23 +0900

    220201 백준 코딩테스트 준비 코드 업로드(DFS와 BFS)

---
+++ Python3/220201/1_1260_DFSBFS.py
@@ -0,0 +1,31 @@
+from collections import deque
+N,M,v=map(int,input().split())
+graph=[[0]*(N+1) for _ in range(N+1)]
+visited=[0]*(N+1)
+for _ in range(M):
+    x,y=map(int,input().split())
+    graph[x][y]=graph[y][x]=1
+
+def DFS(v):
+    visited[v]=1
+    print(v,end=' ')
+    for i in range(1,N+1):
+        if visited[i]==0 and graph[v][i]==1:
+            DFS(i)
+
+def BFS(v):
+    visited=[0]*(N+1)
+    Q=deque()
+    Q.append(v)
+    visited[v]=1
+    while Q:
+        v=Q.popleft()
+        print(v,end=' ')
+        for i in range(1,N+1):
+            if visited[i]==0 and graph[v][i]==1:
+                Q.append(i)
+                visited[i]=1
+                
+DFS(v)
+print()
+BFS(v)
